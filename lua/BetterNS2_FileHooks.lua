ae361051c559e75db85861a1d8f846b5ce4bc876
xGolden <brownchrisg@gmail.com>
2021-02-09 10:17:10 -0500

    Initial mod and menu setup (#2)

---
+++ lua/BetterNS2_FileHooks.lua
@@ -0,0 +1,2 @@
+ModLoader.SetupFileHook("lua/menu2/NavBar/Screens/Options/Mods/ModsMenuData.lua", "lua/ModsMenuData.lua", "post")
+ModLoader.SetupFileHook("lua/GUIFeedback.lua", "lua/GUI/GUIFeedback.lua", "post")
